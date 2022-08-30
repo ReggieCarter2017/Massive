13be49d543e8dcdf5b87b4534d325134573d681b
coolpagani <coolpagani@gmail.com>
2022-08-30 18:30:40 +0300

    Закончили 13 задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿int numbers = new Random().Next(100, 1000);
-Console.Write($"{numbers} -> {numbers.ToString()[index: 1]}");
+﻿int numbers = new Random().Next(1, 10000);
+int count = numbers.ToString().Length;
+    if (count >= 3)
+    Console.Write($"{numbers} -> {numbers.ToString()[index: 2]}");
+    else
+    Console.Write("Третьей цифры нет!");
